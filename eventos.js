4713bdd7504e0a516e787e312e3632a06fe8dce6
professorisidro <isidro@professorisidro.com.br>
2021-06-10 09:49:21 -0300

    Relatório de eventos

--- eventos.js
+++ eventos.js
@@ -1,3 +1,25 @@
 function gerarRelatorio(){
-    
+    var txtIni = document.getElementById("txtDataIni").value;
+    var txtFim = document.getElementById("txtDataFim").value;
+
+    console.log("Datas capturadas "+txtIni+" / "+txtFim);
+
+    /*
+    passos: 
+    1 - enviar os dados para o backend
+    2 - ao chegar a resposta, precisamos extrair o JSON dela -> isso deve trazer uma lista de eventos
+    3 - gerar dinamicamente uma tabela com esses valores da lista
+    */
+    /*
+    res = fetch(...)
+    lista = res.json()
+    console.log(lista);
+    */
+    fetch("http://localhost:8080/eventosporperiodo?inicio="+txtIni+"&fim="+txtFim)
+        .then(res => res.json())
+        .then(lista => preencheRelatorio(lista));
+}
+
+function preencheRelatorio(lista){
+
 }
